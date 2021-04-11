e06f2f581eb99e1000a219d382173906170485c2
Sai Kota <skota@linkedin.com>
2021-04-10 23:15:13 -0700

    adding azure functions

---
+++ api/GetTasks/index.js
@@ -0,0 +1,86 @@
+module.exports = async function (context, req) {
+    context.res = {
+      body: [
+          {
+            "id": 1,
+            "name": "API mapping",
+            "author": "skota",
+            "isComplete": true
+          },
+          {
+            "id": 2,
+            "name": "Mirage API Data",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 3,
+            "name": "Ember Fire",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 4,
+            "name": "Ember Data",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 5,
+            "name": "PWA",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 6,
+            "name": "Integrate with Azure Functions",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 7,
+            "name": "Rearrange and persist list",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 8,
+            "name": "Create Task",
+            "author": "skota",
+            "isComplete": true
+          },
+          {
+            "id": 9,
+            "name": "Have Multiple Users",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "id": 10,
+            "name": "Have Authorization Support",
+            "author": "skota",
+            "isComplete": false
+          },
+          {
+            "name": "Add Footer",
+            "author": "skota",
+            "id": 11
+          },
+          {
+            "name": "Update a Task",
+            "author": "skota",
+            "id": 12
+          },
+          {
+            "name": "Refresh the list once user submits data",
+            "author": "skota",
+            "id": 13
+          },
+          {
+            "name": "Update Readme with json-server details",
+            "author": "skota",
+            "id": 14
+          }
+        ]
+    };
+  };
